8fccfc2e1c10498d5c4f4980727a55ff78ecdbf8
Rémy <remyc59440@hotmail.com>
2022-11-22 10:47:28 +0100

    exo 73 ok

--- exo 1/exercice1.js
+++ exo 1/exercice1.js
@@ -1,5 +1,15 @@
-let avatarEls = document.getElementsByTagName("img");
+let avatarEls = document.querySelectorAll("img");
 
+console.log(avatarEls);
+console.log(typeof avatarEls);
+
+
+for(let i = 0; i < avatarEls.length; i++){
+    avatarEls[i].innerText = i.toString();
+    avatarEls[i].style.height = "50px"
+    avatarEls[i].title = "test"
+    avatarEls[i].alt = "test-alt";
+}
 
 
 
